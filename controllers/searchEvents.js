4672f7acc54f307699734a56683cd9643d97aff8
Parker <parkerproject@gmail.com>
2016-02-05 14:44:20 -0500

    updates

--- controllers/searchEvents.js
+++ controllers/searchEvents.js
@@ -1,82 +1,82 @@
-require('dotenv').load();
-var collections = ['events'];
-var db = require("mongojs").connect(process.env.MONGODB_URL, collections);
-var Joi = require('joi');
-var _ = require('lodash');
-var server = require('../server');
-var dist = require('../helpers/dist');
-
+require('dotenv').load()
+var collections = ['events']
+var db = require('mongojs').connect(process.env.MONGODB_URL, collections)
+var Joi = require('joi')
+var _ = require('lodash')
+var server = require('../server')
+var dist = require('../helpers/dist')
 
 function _prepareQueryObject(radius, location) {
   if (location) {
+    var lng = location.split(',')[0]
+    var lat = location.split(',')[1]
+
     return {
       $geoWithin: {
         $centerSphere: [
-                  [location[0], location[1]], radius
-              ]
+          [Number(lng), Number(lat)], radius
+        ]
       }
 
-    };
+    }
   } else {
-    return {};
+    return {}
   }
 }
 
 module.exports = {
-
   index: {
     handler: function (request, reply) {
-
-      "use strict";
+      'use strict'
 
       if (!request.query.key || request.query.key !== process.env.API_KEY) {
-        reply('You are not authorized');
+        reply('You are not authorized')
       }
 
-      var q = decodeURIComponent(request.query.q);
-      var limit = request.query.limit || 20;
-      var skip = request.query.offset || 0;
-      var _geo = request.query.geo;
-      var accuracy = 1; // accuracy for computing geo distance
-      var queryObj = {};
-      q = q.trim();
-
+      var q = decodeURIComponent(request.query.q)
+      var limit = request.query.limit || 20
+      var skip = request.query.offset || 0
+      var _geo = request.query.geo
+      var accuracy = 1 // accuracy for computing geo distance
+      var queryObj = {}
+      q = q.trim()
 
       queryObj.$text = {
         $search: q
-      };
+      }
 
+      console.log(request.query.geo)
 
       new Promise(function (resolve) {
-        var _radius = 5 / 3959; // radius in 5 miles
+        var _radius = 5 / 3959 // radius in 5 miles
         if (request.query.geo) {
-          queryObj.loc = _prepareQueryObject(_radius, request.query.geo);
+          queryObj.loc = _prepareQueryObject(_radius, request.query.geo)
         }
-        resolve();
+        resolve()
       }).then(function () {
         return new Promise(function (resolve) {
           db.events.find(queryObj, {
             score: {
-              $meta: "textScore"
+              $meta: 'textScore'
             }
           }).skip(skip).sort({
             score: {
-              $meta: "textScore"
+              $meta: 'textScore'
             }
           }).limit(limit, function (err, results) {
-            var res = Array.isArray(results) ? results : [];
+            var res = Array.isArray(results) ? results : []
 
             if (_geo && parseFloat(_geo[0]) && parseFloat(_geo[1])) {
               res = res.sort(function (d1, d2) {
-                return dist(d1.loc.coordinates, _geo, accuracy) - dist(d2.loc.coordinates, _geo, accuracy);
-              });
+                return dist(d1.loc.coordinates, _geo, accuracy) - dist(d2.loc.coordinates, _geo, accuracy)
+              })
             }
-            resolve(res);
-          });
-        });
+            resolve(res)
+          })
+        })
       }).then(function (res) {
-        reply(res);
-      });
+        reply(res)
+      })
     },
     description: 'Search events',
     notes: 'search events using any keywords',
@@ -93,4 +93,4 @@ module.exports = {
 
   }
 
-};
+}
