b338b0ca744d05728ac4a17d90f4afe565505621
Parker <parkerproject@gmail.com>
2016-04-04 00:33:54 -0400

    updates

--- controllers/members.js
+++ controllers/members.js
@@ -5,7 +5,6 @@ var db = mongojs.connect(process.env.MONGODB_URL, collections)
 var Joi = require('joi')
 var _ = require('lodash')
 var Promise = require('es6-promise').Promise
-
 module.exports = {
   index: {
     handler: function (request, reply) {
@@ -17,12 +16,10 @@ module.exports = {
       var limit = request.query.limit || 20
       var user = {}
       var count, data
-
       if (request.query.user_id) {
         user.user_id = request.query.user_id
       }
       user.type_id = request.query.type_id
-
       new Promise(function (resolve) {
         db.members.count(user, function (err, res) {
           count = res
@@ -47,7 +44,6 @@ module.exports = {
     description: 'Get members of a group or event',
     notes: 'Returns members of a group or event, checks if a user is in a group or event',
     tags: ['api'],
-
     validate: {
       query: {
         key: Joi.string().required().description('API key to access data'),
@@ -57,16 +53,13 @@ module.exports = {
         offset: Joi.number().integer().description('defaults to 0'),
       }
     }
-
   },
-
   add: {
     handler: function (request, reply) {
       'use strict'
       if (!request.payload.key || request.payload.key !== process.env.API_KEY) {
         reply('You are not authorized')
       }
-
       var user = {
         user_id: request.payload.user_id,
         picture_id: request.payload.picture_id,
@@ -74,7 +67,6 @@ module.exports = {
         type: request.payload.type,
         type_id: request.payload.type_id
       }
-
       db.members.find({
         user_id: request.payload.user_id,
         type_id: request.payload.type_id
@@ -92,13 +84,11 @@ module.exports = {
             message: 'Already a member'
           })
         }
-
       })
     },
     description: 'Add member to group or event',
     notes: 'This will add a member to a group or an event',
     tags: ['api'],
-
     validate: {
       payload: {
         key: Joi.string().required().description('API key to access data'),
@@ -109,31 +99,25 @@ module.exports = {
         name: Joi.string().required().description('name of the user')
       }
     }
-
   },
-
   update: {
     handler: function (request, reply) {
       'use strict'
       if (!request.payload.key || request.payload.key !== process.env.API_KEY) {
         reply('You are not authorized')
       }
-
       let user = {}
       let member = {}
       let channel_user = {}
-
       if (request.payload.picture_id) {
         user.creator_picture = request.payload.picture_id
         member.picture_id = request.payload.picture_id
-        chann.picture_id = request.payload.picture_id
+        channel_user.picture_id = request.payload.picture_id
       }
-
       if (request.payload.name) {
         member.name = request.payload.name
         user.creator_name = request.payload.name
       }
-
       db.members.update({
         user_id: request.payload.user_id
       }, {
@@ -159,7 +143,6 @@ module.exports = {
             if (request.payload.picture_id) {
               let userEvents = ['event', 'group', 'event_joined', 'group_joined', 'comment']
               let userEvents_2 = ['follow', 'group_invite', 'event_invite', 'reply']
-
               let queryType = {
                 user_id: request.payload.user_id,
                 activity_type: {
@@ -190,17 +173,13 @@ module.exports = {
                 message: 'User has been updated'
               })
             }
-
           })
         })
-
       })
-
     },
     description: 'Update a member in all groups and events',
     notes: "This will update a member's info in all groups and events",
     tags: ['api'],
-
     validate: {
       payload: {
         key: Joi.string().required().description('API key to access data'),
@@ -210,33 +189,27 @@ module.exports = {
         name: Joi.string().description('name of the user')
       }
     }
-
   },
-
   remove: {
     handler: function (request, reply) {
       'use strict'
       if (!request.payload.key || request.payload.key !== process.env.API_KEY) {
         reply('You are not authorized')
       }
-
       var user = {
         user_id: request.payload.user_id,
         type_id: request.payload.type_id
       }
-
       db.members.remove(user, function () {
         reply({
           status: 1,
           message: 'User has been removed'
         })
       })
-
     },
     description: 'Remove member from group or event',
     notes: 'This will remove a member from a group or an event',
     tags: ['api'],
-
     validate: {
       payload: {
         key: Joi.string().required().description('API key to access data'),
@@ -244,7 +217,5 @@ module.exports = {
         user_id: Joi.string().required().description('id of the user')
       }
     }
-
   }
-
 }
